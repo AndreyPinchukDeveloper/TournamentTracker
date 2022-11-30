d18104e188542545b56fb15e63252b2354f3da85
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-11-30 23:52:23 +0700

    Development Update #4
    - has created Database and all tables to keep data;
    - has created IDataConnection interface for the future classes which shall work with data.

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Data;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+
+        }
+    }
+}
