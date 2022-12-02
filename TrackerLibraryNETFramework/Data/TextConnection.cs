8ec396fe0b00639c9fb3d3e09b0b53aa34a28d9d
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-12-02 21:13:29 +0700

    HotFix
    - has transfered all library(data/models) based on .NET Core to new library project based on .NET Framework.

---
+++ TrackerLibraryNETFramework/Data/TextConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibraryNETFramework.Models;
+
+namespace TrackerLibraryNETFramework.Data
+{
+    public class TextConnecion : IDataConnection
+    {
+        // TODO - wire up the createPrize for text files
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+
+            return model;
+        }
+    }
+}
---
+++ TrackerLibraryNETFramework/GlobalConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibraryNETFramework.Data;
+
+namespace TrackerLibraryNETFramework
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO - create the sql connection
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+
+            if (textFiles)
+            {
+                // TODO - create the test connection
+                TextConnecion text = new TextConnecion();
+                Connections.Add(text);
+            }
+        }
+    }
+}
