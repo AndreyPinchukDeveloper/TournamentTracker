6a75c0ec6cfabd7e81f49ca84ea60d6cde84170c
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-01-07 08:42:49 +0700

    Development Update #32
    - error handler for data validation(AddRegVM + INotifyDataErrorInfo).

--- GameProgressTracker/ViewModels/AddRegistrationViewModel.cs
+++ GameProgressTracker/ViewModels/AddRegistrationViewModel.cs
@@ -3,7 +3,9 @@ using GameProgressTracker.Models;
 using GameProgressTracker.Services;
 using GameProgressTracker.Stores;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +13,9 @@ using System.Windows.Input;
 
 namespace GameProgressTracker.ViewModels
 {
-    public class AddRegistrationViewModel : ViewModelBase
+    public class AddRegistrationViewModel : ViewModelBase, INotifyDataErrorInfo
     {
+        #region Fields
         private string _gameID;
 
         public string GameID
@@ -44,7 +47,7 @@ namespace GameProgressTracker.ViewModels
             }
         }
 
-        private DateTime _startTime;
+        private DateTime _startTime = new DateTime(2023, 1, 1);
 		public DateTime StartTime
         {
             get { return _startTime; }
@@ -55,26 +58,48 @@ namespace GameProgressTracker.ViewModels
             }
         }
 
-        private DateTime _endTime;
+        private DateTime _endTime = new DateTime(2023, 1, 1);
+  
         public DateTime EndTime
         {
             get { return _endTime; }
             set
             {
+                _proretyNameToErrorsDictionary.Remove(nameof(EndTime));
+
+                if (EndTime<StartTime)
+                {
+                    List<string> endDataErrors = new List<string>()
+                    {
+                        "The end date can't be before start date."
+                    };
+                    _proretyNameToErrorsDictionary.Add(nameof(EndTime), endDataErrors);
+                    ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(EndTime)));
+                }
+
                 _endTime = value;
                 OnPropertyChanged(nameof(EndTime));
             }
         }
+        #endregion
 
-        #region Commands
         public ICommand SubmitButtonCommand { get; }
         public ICommand CancelButtonCommand { get; }
-        #endregion
+
+        private readonly Dictionary<string, List<string>> _proretyNameToErrorsDictionary;
+
+        public bool HasErrors => _proretyNameToErrorsDictionary.Any();
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;//when we add error we call this event
 
         public AddRegistrationViewModel(GamesStore gameStore, NavigationService navigationService)
         {
             SubmitButtonCommand = new AddRegistrationCommand(this, gameStore, navigationService);
             CancelButtonCommand = new NavigateCommand(navigationService);
+        }
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return _proretyNameToErrorsDictionary.GetValueOrDefault(propertyName, new List<string>());
         }
     }
 }
