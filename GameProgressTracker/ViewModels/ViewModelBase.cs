fe5b22a99e6c4733d223ed466dbee9bd876086dd
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-01-16 22:22:00 +0700

    Development Update 38
    - some small chages;
    - add Set method into ViewModelBase to check propertie's value, to avoid circular updating without necessary.

--- GameProgressTracker/ViewModels/ViewModelBase.cs
+++ GameProgressTracker/ViewModels/ViewModelBase.cs
@@ -2,20 +2,41 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GameProgressTracker.ViewModels
 {
-    public class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public virtual void Dispose() { }
+
+        /// <summary>
+        /// We need this method to avoid updating any properties if values are the same
+        /// 
+        /// the bust way when there are happen circular updating
+        /// 
+        /// without it we update all
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="PropertyName"></param>
+        /// <returns></returns>
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
     }
 }
